b1b25e2ef1c572e6507c6a1b1443c97e167d9e4a
Shutemov <shutemov1997@mail.ru>
2020-11-03 13:03:23 +0300

    Обновляет интерфейс IPlayer
    Описывает функции плеера

--- src/interfaces/IPlayer.java
+++ src/interfaces/IPlayer.java
@@ -3,4 +3,19 @@ package interfaces;
 import classes.SoundContent;
 
 public interface IPlayer {
+    void play();
+
+    void stop();
+
+    SoundContent next();
+
+    SoundContent prev();
+
+    void upVolume();
+
+    void downVolume();
+
+    void setSoundContent(SoundContent soundContent);
+
+    void deleteSoundContent(SoundContent soundContent);
 }
