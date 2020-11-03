fd761adb9a08144d5adf7983a3e1e99f8c4dd0ec
Shutemov <shutemov1997@mail.ru>
2020-11-03 22:05:33 +0300

    Добавляет методы добавления контента в списки MyCollection

--- src/classes/MyCollection.java
+++ src/classes/MyCollection.java
@@ -3,37 +3,37 @@ package classes;
 import java.util.ArrayList;
 
 public class MyCollection {
-    ArrayList<Track> tracks;
+    ArrayList<SoundContent> tracks;
     ArrayList<Playlist> playlists;
     ArrayList<Artist> artists;
 
-    public MyCollection(ArrayList<Track> tracks, ArrayList<Playlist> playlists, ArrayList<Artist> artists) {
+    public MyCollection(ArrayList<SoundContent> tracks, ArrayList<Playlist> playlists, ArrayList<Artist> artists) {
         this.tracks = tracks;
         this.playlists = playlists;
         this.artists = artists;
     }
 
-    public ArrayList<Track> getTracks() {
-        return tracks;
-    }
+    void addSoundContent(SoundContent soundContent){
+        tracks.add(soundContent);
+    };
 
-    public void setTracks(ArrayList<Track> tracks) {
-        this.tracks = tracks;
+    void addPlaylist(Playlist playlist){
+        playlists.add(playlist);
+    };
+
+    void addArtist(Artist artist){
+        artists.add(artist);
+    };
+
+    public ArrayList<SoundContent> getTracks() {
+        return tracks;
     }
 
     public ArrayList<Playlist> getPlaylists() {
         return playlists;
     }
 
-    public void setPlaylists(ArrayList<Playlist> playlists) {
-        this.playlists = playlists;
-    }
-
     public ArrayList<Artist> getArtists() {
         return artists;
-    }
-
-    public void setArtists(ArrayList<Artist> artists) {
-        this.artists = artists;
     }
 }
