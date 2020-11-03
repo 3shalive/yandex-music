f061e04a863ab92de4a874de3d90a9d4db2236c3
Shutemov <shutemov1997@mail.ru>
2020-11-03 22:51:07 +0300

    Инициализирует "сервер" данными
    Добавлет метод инициализации, который формирует списки треков, артистов и плейлистов

--- src/app/Server.java
+++ src/app/Server.java
@@ -6,6 +6,33 @@ import interfaces.IServer;
 import java.util.ArrayList;
 
 public class Server implements IServer {
+    private ArrayList<SoundContent> soundContents = new ArrayList<SoundContent>();
+    private ArrayList<Artist> artists = new ArrayList<Artist>();
+    private ArrayList<Playlist> playlists = new ArrayList<Playlist>();
+
+    Server() {
+        init();
+    }
+
+    void init() {
+        // создаем артистов и их треки
+        for (int i = 0; i < 10; i++) {
+            Artist artist = new Artist("Artist: " + i);
+            for (int j = 0; j < 10; j++) {
+                SoundContent soundContent = new SoundContent(artist, "1:23", "soundContent " + j + " by " + artist.getName());
+                soundContents.add(soundContent);
+            }
+            artists.add(artist);
+        }
+
+        // создаем плейлисты
+        for (int i = 0; i < 10; i++) {
+            Albom albom = new Albom("albom " + i, "1997 " + i, "Rock " + i);
+            Playlist playlist = new Playlist(this.soundContents, albom);
+            playlists.add(playlist);
+        }
+    }
+
     @Override
     public User getUser() {
         return new User("User1");
@@ -13,43 +40,16 @@ public class Server implements IServer {
 
     @Override
     public ArrayList<Playlist> getPlaylists() {
-        // создаем мок данные
-        Albom albom1 = new Albom("Albom 1", "1997", "Rock");
-        ArrayList<SoundContent> soundContents = new ArrayList<SoundContent>();
-        SoundContent soundContent1 = new SoundContent(new Artist("Artist 1"), "1:23", "soundContent1");
-        soundContents.add(soundContent1);
-
-        Playlist playlist1 = new Playlist(soundContents, albom1);
-
-        ArrayList<Playlist> playlists = new ArrayList<>();
-
-        playlists.add(playlist1);
         return playlists;
     }
 
     @Override
     public ArrayList<Artist> getArtists() {
-        ArrayList<Artist> artists = new ArrayList<Artist>();
-
-        for (int i = 0; i < 10; i++) {
-            Artist artist = new Artist("Artist " + i);
-            artists.add(artist);
-        }
-
         return artists;
     }
 
     @Override
     public ArrayList<SoundContent> getSoundContents() {
-        ArrayList<SoundContent> soundContents = new ArrayList<SoundContent>();
-
-        ArrayList<Artist> artists = getArtists();
-
-        for (int i = 0; i < 10; i++) {
-            SoundContent soundContent = new SoundContent(artists.get(i), "1:23", "sound content " + i);
-            soundContents.add(soundContent);
-        }
-
         return soundContents;
     }
 }
