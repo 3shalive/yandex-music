a1c727cb6bfce4f0d045394c277e7ebd3af08865
Shutemov <shutemov1997@mail.ru>
2020-11-04 19:08:05 +0300

    Тестирует работу моделей и GUI

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,10 +1,31 @@
 package app;
 
-import app.Server;
+import GUI.CreateArtistForm;
+import classes.Albom;
+import classes.Artist;
+
+
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Start yandex-app");
         Server server = new Server();
+
+        // test
+        System.out.println("create model test");
+        Artist artist = server.createArtist("new Artist");
+        System.out.println(artist.getName());
+
+        ArrayList<Albom> alboms = new ArrayList<Albom>();
+        alboms.add(new Albom(server.genPlaylistId(), "Playlist 1", "1997", "Rap", artist));
+
+        artist.setAlboms(alboms);
+
+        System.out.println(artist.getAlboms().get(0).getGenre());
+
+        //test GUI
+        new CreateArtistForm();
+
     }
 }
