87c9d0b4658d421d51924fc0aff396a8e45fd2e8
Shutemov <shutemov1997@mail.ru>
2020-11-04 14:36:17 +0300

    Реализует ICreateService и IDeleteService

--- src/app/Server.java
+++ src/app/Server.java
@@ -6,32 +6,63 @@ import interfaces.*;
 import java.util.ArrayList;
 
 public class Server implements IServer, ICreateService, IReadService, IUpdateService, IDeleteService {
+    // списки выступают в роли хранилища (т.е. "БД")
     private ArrayList<SoundContent> soundContents = new ArrayList<SoundContent>();
     private ArrayList<Artist> artists = new ArrayList<Artist>();
     private ArrayList<Playlist> playlists = new ArrayList<Playlist>();
 
+    //Заполняем хранилище фейковыми данными
     Server() {
         init();
     }
 
+    // функция генерации фейковых данных
     void init() {
         // создаем артистов и их треки
         for (int i = 0; i < 10; i++) {
-            Artist artist = new Artist("Artist: " + i);
+            int artistId = genArtistId();
+            Artist artist = new Artist(artistId, "Artist: " + i);
             for (int j = 0; j < 10; j++) {
-                SoundContent soundContent = new SoundContent(artist, "1:23", "soundContent " + j + " by " + artist.getName());
+                int soundContentId = genSoundContentId();
+                SoundContent soundContent = new SoundContent(soundContentId, artist, "1:23", "soundContent " + j + " by " + artist.getName());
                 soundContents.add(soundContent);
             }
             artists.add(artist);
         }
 
         // создаем плейлисты
-        for (int i = 0; i < 10; i++) {
-            Albom albom = new Albom("albom " + i, "1997 " + i, "Rock " + i);
-            Playlist playlist = new Playlist(this.soundContents, albom);
-            playlists.add(playlist);
+        for(Artist artist: artists){
+            for (int i = 0; i < 10; i++) {
+                int albomId = genPlaylistId();
+                int playlistId = genPlaylistId();
+                Albom albom = new Albom(albomId, "albom " + i, "1997 " + i, "Rock " + i, artist);
+                Playlist playlist = new Playlist(playlistId, this.soundContents, albom);
+                playlists.add(playlist);
+            }
         }
     }
+
+    // наивные генераторы id для сущностей
+    int genSoundContentId() {
+        int id = soundContents.size() + 1;
+        return id;
+    }
+
+    ;
+
+    int genPlaylistId() {
+        int id = playlists.size() + 1;
+        return id;
+    }
+
+    ;
+
+    int genArtistId() {
+        int id = artists.size() + 1;
+        return id;
+    }
+
+    ;
 
     @Override
     public User getUser() {
@@ -57,18 +88,40 @@ public class Server implements IServer, ICreateService, IReadService, IUpdateSer
         Create
      */
     @Override
-    public SoundContent createSoundContent() {
-        return null;
+    public SoundContent createSoundContent(Artist artist, String time, String name) {
+        // создаем soundContent
+        int id = soundContents.size() + 1;
+        SoundContent soundContent = new SoundContent(id, artist, time, name);
+
+        // "сохраняем" его на сервере
+        soundContents.add(soundContent);
+
+        // возвращаем созданный экземпляр
+        return soundContent;
     }
 
     @Override
-    public Playlist createPlaylist() {
-        return null;
+    public Playlist createPlaylist(ArrayList<SoundContent> soundContents, Albom albom) {
+        int id = playlists.size() + 1;
+        Playlist playlist = new Playlist(id, soundContents, albom);
+
+        // "сохраняем" его на сервере
+        playlists.add(playlist);
+
+        // возвращаем созданный экземпляр
+        return playlist;
     }
 
     @Override
-    public Artist createArtist() {
-        return null;
+    public Artist createArtist(String name) {
+        int id = artists.size() + 1;
+        Artist artist = new Artist(id, name);
+
+        // "сохраняем" его на сервере
+        artists.add(artist);
+
+        // возвращаем созданный экземпляр
+        return artist;
     }
 
     /*
@@ -76,12 +129,12 @@ public class Server implements IServer, ICreateService, IReadService, IUpdateSer
     */
     @Override
     public Boolean deletePlaylistById(int id) {
-        return null;
+        return playlists.removeIf(e -> (e.getId() == id));
     }
 
     @Override
     public Boolean deleteArtisById(int id) {
-        return null;
+        return artists.removeIf(e -> (e.getId() == id));
     }
 
     @Override
@@ -91,7 +144,7 @@ public class Server implements IServer, ICreateService, IReadService, IUpdateSer
 
     @Override
     public Boolean deleteSoundContentById(int id) {
-        return null;
+        return soundContents.removeIf(e -> (e.getId() == id));
     }
 
     /*
@@ -99,41 +152,56 @@ public class Server implements IServer, ICreateService, IReadService, IUpdateSer
     */
     @Override
     public SoundContent getSoundContentById(int id) {
+        for (SoundContent soundContent : soundContents) {
+            return soundContent.getId() == id ? soundContent : null;
+        }
         return null;
     }
 
     @Override
-    public ArrayList<Playlist> getAllSoundContent() {
-        return null;
+    public ArrayList<SoundContent> getAllSoundContent() {
+        return soundContents;
     }
 
     @Override
     public Playlist getPlaylistById(int id) {
+        for (Playlist playlist : playlists) {
+            return playlist.getId() == id ? playlist : null;
+        }
         return null;
     }
 
     @Override
     public Playlist getPlaylistByArtist(Artist artist) {
+        for (Playlist playlist : playlists) {
+            return playlist.getAlbom().getArtist() == artist ? playlist : null;
+        }
         return null;
     }
 
     @Override
     public Playlist getPlaylistByAlbom(Albom albom) {
+        for (Playlist playlist : playlists) {
+            return playlist.getAlbom() == albom ? playlist : null;
+        }
         return null;
     }
 
     @Override
     public ArrayList<Playlist> getAllPlaylists() {
-        return null;
+        return playlists;
     }
 
     @Override
     public Artist getArtistByName(String name) {
+        for (Artist artist : artists) {
+            return artist.getName().equals(name) ? artist : null;
+        }
         return null;
     }
 
     @Override
-    public ArrayList<Playlist> getAllArtist() {
-        return null;
+    public ArrayList<Artist> getAllArtist() {
+        return artists;
     }
 }
