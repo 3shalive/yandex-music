2aa458d8a03869ff93d4eeb33066721df13dbfd8
Shutemov <shutemov1997@mail.ru>
2020-11-04 19:17:16 +0300

    Чистит класс
    Оставляет только инициализацию GUI

--- src/app/Main.java
+++ src/app/Main.java
@@ -12,20 +12,7 @@ public class Main {
         System.out.println("Start yandex-app");
         Server server = new Server();
 
-        // test
-        System.out.println("create model test");
-        Artist artist = server.createArtist("new Artist");
-        System.out.println(artist.getName());
-
-        ArrayList<Albom> alboms = new ArrayList<Albom>();
-        alboms.add(new Albom(server.genPlaylistId(), "Playlist 1", "1997", "Rap", artist));
-
-        artist.setAlboms(alboms);
-
-        System.out.println(artist.getAlboms().get(0).getGenre());
-
         //test GUI
-        new CreateArtistForm();
-
+        new CreateArtistForm(server);
     }
 }
