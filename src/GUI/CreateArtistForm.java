642bb9d6b93338728bdbb08a3aba7b718b727352
Shutemov <shutemov1997@mail.ru>
2020-11-04 19:17:50 +0300

    Добавлет событие закрытия окна

--- src/GUI/CreateArtistForm.java
+++ src/GUI/CreateArtistForm.java
@@ -1,6 +1,12 @@
 package GUI;
 
+import app.Server;
+import classes.Artist;
+import interfaces.ICreateService;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CreateArtistForm extends JFrame{
     private JPanel panel1;
@@ -8,10 +14,30 @@ public class CreateArtistForm extends JFrame{
     private JButton closeButton;
     private JButton createButton;
 
-    public CreateArtistForm(){
+    public CreateArtistForm(Server server){
+        ICreateService createService = server;
+
         setContentPane(panel1);
         setVisible(true);
-
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        createButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String artistName = artistNameField.getText();
+
+                System.out.println(artistName);
+
+                Artist createdArtist = createService.createArtist(artistName);
+
+                System.out.println(createdArtist.getId());
+            }
+        });
+        closeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
     }
 }
